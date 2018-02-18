3b852f3d372866b49c624b6e129e621b0cf18738
sidearrow <sidearrow1219@gmail.com>
2018-02-18 22:32:07 +0900

    done

---
+++ C++/0008.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    while (true) {
+        int x;
+        cin >> x;
+        if (cin.eof()) {
+            break;
+        }
+        if (x > 36) {
+            cout << 0 << endl;
+            continue;
+        }
+        int i = 0;
+        for (int a = 0; a < 10 && x - a >= 0; a++) {
+            int _a = x - a;
+            if (_a > 27) {
+                continue;
+            }
+            for (int b = 0; b < 10 && _a - b >= 0; b++) {
+                int _b = _a - b;
+                if (_b > 18) {
+                    continue;
+                }
+                for (int c = 0; c < 10 && _b - c >= 0; c++) {
+                    int _c = _b - c;
+                    if (_c > 9) {
+                        continue;
+                    }
+                    i++;
+                }
+            }
+        }
+        cout << i << endl;
+    }
+}
