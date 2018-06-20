95d402dba1d5468e49b6b9cced4d16fab8d6e527
sidearrrow <sidearrow1219@gmail.com>
2018-06-20 15:36:53 +0900

    commit

--- aoj/C++/0005.cpp
+++ aoj/C++/0005.cpp
@@ -1,5 +1,5 @@
-#include<cstdio>
-#include<iostream>
+#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
--- aoj/Java/0002.java
+++ aoj/Java/0002.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 
-class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()) {
-            int a = String.valueOf(sc.nextInt() + sc.nextInt()).length();
-            System.out.println(a);
-        }
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while(sc.hasNext()) {
+      int a = String.valueOf(sc.nextInt() + sc.nextInt()).length();
+      System.out.println(a);
     }
-}
+  }
